4c34171c26acc2ea981e9dfd85b6fe1cf5f47960
nordicway <zielkeb@zielkeb.de>
2016-06-04 14:23:32 +0200

    added test case to debug encoding issues

--- src/test/java/it/zielke/moji/EncodingTest.java
+++ src/test/java/it/zielke/moji/EncodingTest.java
@@ -2,6 +2,8 @@ package it.zielke.moji;
 
 import static org.junit.Assert.fail;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
 import java.util.Locale;
 
@@ -28,6 +30,41 @@ public class EncodingTest {
 		if (!localeAvailable) {
 			fail("No US Locale available");
 		}
+	}
+
+	@Test
+	public void testDefaultCharset() {
+		try {
+			System.out.println("Java version: "
+					+ System.getProperty("java.version"));
+			System.out.println(String.format(
+					"Java VM:\n\tvendor: %s\n\tname: %s",
+					System.getProperty("java.vm.vendor"),
+					System.getProperty("java.vm.name")));
+			System.out.println(String.format(
+					"OS:\n\tname: %s\n\tversion: %s\n\tarch: %s",
+					System.getProperty("os.name"),
+					System.getProperty("os.version"),
+					System.getProperty("os.arch")));
+			System.out.println("Encoding:");
+			System.out
+					.println("\tDefault Charset: " + Charset.defaultCharset());
+			System.out.println("\tfile.encoding: "
+					+ System.getProperty("file.encoding"));
+			System.out.println("\tDefault Charset in Use: "
+					+ getDefaultCharSet());
+		} catch (SecurityException e) {
+			System.out.println("WARN - Running under security manager: "
+					+ e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	private static String getDefaultCharSet() {
+		OutputStreamWriter writer = new OutputStreamWriter(
+				new ByteArrayOutputStream());
+		String enc = writer.getEncoding();
+		return enc;
 	}
 
 }
